4c0752afaa514af212a49b4f54fe7c3f6f149e20
dan-l <daniel9618@gmail.com>
2014-10-25 22:20:43 -0700

    Integrated backbone into app, with Backbone View, Model and Collection being the main ones we will be using for now. Will revise file structure to increase the 'cohesiveness' of our app

--- Server.js
+++ Server.js
@@ -1,37 +1,38 @@
-// Server
-var databaseRef = new Firebase("https://vivid-torch-5902.firebaseio.com/");
-var currentUser = {};
+var server = (function() {
+  // Server
+  var databaseRef = new Firebase("https://vivid-torch-5902.firebaseio.com/");
+  var currentUser = {};
 
-var Server = {
-  login: function(callback) {
-      databaseRef.authWithOAuthPopup("facebook", function(error, authData) {
-          if(error) console.log('error');
-          if(authData) { 
-            if(typeof callback === 'function') {
-              currentUser = authData;
-              callback(authData);
-            }            
-          }
-        }, {
-          remember: "sessionOnly",
-          scope: "email"
-      });
-      
+  return {
+    login: function(callback) {
+        databaseRef.authWithOAuthPopup("facebook", function(error, authData) {
+            if(error) console.log('error');
+            if(authData) {
+              if(typeof callback === 'function') {
+                currentUser = authData;
+                callback(authData);
+              }
+            }
+          }, {
+            remember: "sessionOnly",
+            scope: "email"
+        });
+
+      },
+
+    pushUserData: function(key, val) {
+        databaseRef.child('users').child(key).set(val);
+      },
+
+    getCurrentUser: function() {
+        return currentUser;
     },
 
-  pushUserData: function(key, val) {
-      databaseRef.child('users').child(key).set(val);
+    logout: function() {
+        databaseRef.unauth();
     },
+  };
 
-  getCurrentUser: function() {
-      return currentUser;
-  },
+})();
 
-  logout: function() {
-      databaseRef.unauth();
-  },
-}
-
-
-
-
+module.exports = Backbone.Model.extend(server);
--- UIController.js
+++ UIController.js
@@ -1,57 +1,9 @@
-var map;
-
+var MapView  = require('./MapView');
 // when page is loaded
 $(document).ready( function () {
-  // load map
-  var mapOptions = {
-    center: { lat: 49.2, lng: -123.1},
-    zoom: 11,
-    disableDefaultUI: true
-  };
 
-  // display map on the div with id map-canvas
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+  new MapView();
+  // need a way to trigger other intialization when logged in
 
-  // on login click
-  $("#login-button").click(function () {
-    Server.login(function(userObject) {
-      if(userObject) { // if login is successful (userObject is not null)
-        $('#login-button').hide();
-        $('#overlay').hide();
-        $('#user-label').text(userObject.facebook.displayName)
-          .css("cursor","pointer")
-          .hover(function() {
-            $('#user-label').text('Logout');
-          }, function() {
-            $('#user-label').text(userObject.facebook.displayName);
-          })
-          .click(function () {
-            Server.logout();
-            location.reload();
-          });
-
-        loadMarkers();
-        
-        // push user info to server
-        Server.pushUserData(userObject.uid, userObject.facebook.displayName);
-      }
-    });
-  }); 
 });
 
-// populate the map with markers
-function loadMarkers() {
-  // parse food truck locations from file to a layer
-  var ctaLayer = new google.maps.KmlLayer({
-    url: 'http://napontaratan.com/WheresTheFood/street_food_vendors.kml'});
-
-  // example of on marker click event
-  google.maps.event.addListener(ctaLayer, 'click', function(kmlEvent) {
-    var text = kmlEvent.featureData.name;
-    console.log(text);
-  });
-
-  // adds the layer to the map
-  ctaLayer.setMap(map);
-}
