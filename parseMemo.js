9cf888dacd50b2d51f0656205ed37947ae145cfa
Kijtapart <scc385@gmail.com>
2014-10-28 22:58:32 -0700

    remove old file

--- parseMemo.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("saveButton").addEventListener("click", saveTextToDB);
-
-function saveTextToDB(){
-var typeMemo = document.getElementById("memoBox");
-// need to add restaurant here
-pushUserMemo(restaurant,pushUserMemo);
-}
