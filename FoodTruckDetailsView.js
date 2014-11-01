f2d232a7a5fbb83885c7a7e8cea559f828dfbcda
Napon Taratan <napon@napontaratan.com>
2014-11-01 12:40:45 -0700

    added the UI for Ratings

--- FoodTruckDetailsView.js
+++ FoodTruckDetailsView.js
@@ -1,8 +1,22 @@
 var FoodTruckDetailsView = (function() {
+
+  var stars = document.getElementsByClassName('star');
+
+  function submitRating(item) {
+    var starNumber = item.target.itemNumber;
+    for(var i = 0; i <= starNumber; i++)
+      stars[i].innerHTML = "&#9733;";
+    for(var j = starNumber + 1; j < 5; j++)
+      stars[j].innerHTML = "&#9734;";
+
+    $('#rScore').text(1+starNumber+'/5');
+  }
+
   return Backbone.View.extend({
 
     initialize: function() {
       this.render(1);
+      this.initRatings();
     },
 
     el: '#foodtruckdetails-div',
@@ -14,7 +28,14 @@ var FoodTruckDetailsView = (function() {
       this.template = this.baseTemplate(foodtruck);
       this.$el.html(this.template);
       return this;
-    }
+    },
+
+    initRatings: function() {
+      for(var i = 0; i < stars.length ; i++) {
+        stars[i].itemNumber = i;
+        stars[i].addEventListener("click", submitRating);
+      }
+    },
 
   });
 })();
