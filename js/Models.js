90f009cfdb9b05c6605e498a166e2375f319f406
Napon Taratan <napon@napontaratan.com>
2014-11-20 12:06:58 -0800

    added some ordering by distance code

--- js/Models.js
+++ js/Models.js
@@ -39,6 +39,7 @@ WTF.FoodTruck = (function() {
       lon: 'N/A',
       marker: 'N/A',
       rating: 0,
+      distance: 0,
       isFavourited: false,
       memo: "",
       openHours: 'Not Available',
