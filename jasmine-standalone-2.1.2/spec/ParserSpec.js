7b2f4e1be6a48ccb33bf5befd05075665d296a11
dan-l <daniel9618@gmail.com>
2014-11-23 16:56:00 -0800

    Setup initial parser testing

---
+++ jasmine-standalone-2.1.2/spec/ParserSpec.js
@@ -0,0 +1,17 @@
+describe("Parser", function() {
+
+  var server = WTF.Server;
+
+  describe('Dataset', function() {
+
+    beforeEach(function() {
+      server.fetchDataset();
+    });
+
+    it('should parsed the correct amount of foodtrucks', function() {
+      expect(WTF.FoodTrucks.length).toBe(111);
+    });
+
+  });
+
+});
--- jasmine-standalone-2.1.2/spec/ServerSpec.js
+++
@@ -1,8 +0,0 @@
-describe("Player", function() {
-
-  var server = WTF.Server;
-
-  it("should have the correct amount of foodtrucks", function() {
-  });
-
-});
