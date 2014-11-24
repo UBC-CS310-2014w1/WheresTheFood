dd55755ffc6c713945fe95ed3d5a13c1a852f607
LMalik <lovedeepmalik@gmail.com>
2014-11-23 20:00:53 -0800

    added a test for a foodtruck in the middle

--- jasmine-standalone-2.1.2/spec/ParserSpec.js
+++ jasmine-standalone-2.1.2/spec/ParserSpec.js
@@ -24,6 +24,14 @@ describe("Parser", function() {
       expect(WTF.FoodTrucks.at(0).get('lon')).toBe(-123.1175335);
     });
 
+    it('should parse the 60th food truck with the correct info', function(){
+      expect(WTF.FoodTrucks.at(60).get('id')).toBe("DT59");
+      expect(WTF.FoodTrucks.at(60).get('name')).toBe("Re-Up BBQ");
+      expect(WTF.FoodTrucks.at(60).get('description')).toBe("Pulled Pork");
+      expect(WTF.FoodTrucks.at(60).get('lat')).toBe(49.28350877);
+      expect(WTF.FoodTrucks.at(60).get('lon')).toBe(-123.1203097);
+    });
+
   });
 
 });
