47d597cbfa7e4b5e5777636a80e5a8075993784e
Napon Taratan <napon@napontaratan.com>
2014-11-23 19:39:08 -0800

    added test for first food truck

--- jasmine-standalone-2.1.2/spec/ParserSpec.js
+++ jasmine-standalone-2.1.2/spec/ParserSpec.js
@@ -12,6 +12,14 @@ describe("Parser", function() {
       expect(WTF.FoodTrucks.length).toBe(111);
     });
 
+    it('should parse the first food truck with the correct info', function() {
+	  expect(WTF.FoodTrucks.at(0).get('id')).toBe("C1");
+      expect(WTF.FoodTrucks.at(0).get('name')).toBe("Via Tevere Pizzeria");
+      expect(WTF.FoodTrucks.at(0).get('description')).toBe("Pizza");
+      expect(WTF.FoodTrucks.at(0).get('lat')).toBe(49.28690264);
+      expect(WTF.FoodTrucks.at(0).get('lon')).toBe(-123.1175335);
+    });
+
   });
 
 });
